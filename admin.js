9259f890cab89afd7775300fba0cb389ed09ba67
Flying-Moe <mikkelwm88@gmail.com>
2025-12-16 21:32:02 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -3,7 +3,10 @@ import {
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+  addDoc,
+  updateDoc,
+  deleteDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 import {
   doc,
