bba59a877efbc936b231e4fc96e09d90549d4124
Flying-Moe <mikkelwm88@gmail.com>
2025-12-16 10:33:50 +0100

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const currentPage = location.pathname.split("/").pop() || "index.html";
+  const currentPage =
+    location.pathname.split("/").pop() || "index.html";
 
   document.querySelectorAll("nav a").forEach(link => {
-    const href = link.getAttribute("href");
-    if (href === currentPage) {
+    if (link.getAttribute("href") === currentPage) {
       link.classList.add("active");
     }
   });
