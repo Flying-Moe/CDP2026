59e1dbd9555964c2d4d4239e5f136b8ed7a0d03c
Flying-Moe <mikkelwm88@gmail.com>
2025-12-19 20:31:17 +0100

    Update admin.people.js

--- admin.people.js
+++ admin.people.js
@@ -98,14 +98,14 @@ function namesAreSimilar(a, b) {
       a.displayName.localeCompare(b.displayName, "en", { sensitivity: "base" })
     )
     .forEach(g => {
+       
+              const similarGroups = groupArray.filter(
+  other => other !== g && namesAreSimilar(g, other)
+);
        const canMerge =
   g.birthDates.size > 1 ||
   g.personIds.size > 1 ||
   similarGroups.length > 0;
-
-       const similarGroups = groupArray.filter(
-  other => other !== g && namesAreSimilar(g, other)
-);
 
 let status = "OK";
 let statusClass = "";
