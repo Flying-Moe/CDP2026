e0df74006dbeabc4effdec8449594831a343da27
Flying-Moe <mikkelwm88@gmail.com>
2026-01-05 12:00:28 +0100

    Update stats.js

--- stats.js
+++ stats.js
@@ -518,8 +518,8 @@ if (typeof badge.overlayText === "function" && tierThreshold != null) {
   img.dataset.overlayText = badge.overlayText(tierThreshold);
 } else {
   img.dataset.overlayText = "Not yet unlocked";
-} else 
-} else {
+} 
+ else {
   img.dataset.overlayText = unlocked
     ? badge.description || badge.name
     : "Not yet unlocked";
